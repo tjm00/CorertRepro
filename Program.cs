08b6ee78eac1b5dfa3a0ba6bd3d18eb02f0135d1
tjm00 <20848320+tjm00@users.noreply.github.com>
2018-08-03 15:41:17 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Asn1.X9;
+
+namespace CorertRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X9ECParameters bcCurve = NistNamedCurves.GetByName("P-521");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CorertRepro
+
+Steps to reproduce:
+dotnet publish -c release -r osx-x64 && ./bin/release/netcoreapp2.1/osx-x64/publish/CorertRepro
